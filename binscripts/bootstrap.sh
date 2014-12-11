4446dceae90c2cfff749cdd038fac6b78a92f5e4
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-12-11 12:25:12 +0100

    modifying bootstrap script after having removed the role distinction

--- binscripts/bootstrap.sh
+++ binscripts/bootstrap.sh
@@ -5,4 +5,4 @@ git clone https://gitlab.uni.lu/modules/infrastructure.git .
 sudo pip install $HOME/.resif/src/bin/
 sudo mkdir /usr/local/apps
 sudo chown $USER: /usr/local/apps
-resif user cleaninstall core
+resif cleaninstall
