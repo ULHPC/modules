bc47e24a2bc6d54a9221b5c5136ca9e63cb85524
Sebastien Varrette <sebastien.varrette@uni.lu>
2014-12-12 18:28:44 +0100

    Squashed 'easybuild/easyblocks/' content from commit 3f70b2b
    git-subtree-dir: easybuild/easyblocks
    git-subtree-split: 3f70b2bb93fbb587a2b9be225f5f47584cb2ffe5

---
+++ easybuild/easyblocks/generic/toolchain.py
@@ -0,0 +1,59 @@
+##
+# Copyright 2009-2013 Ghent University
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+"""
+EasyBuild support for installing compiler toolchains, implemented as an easyblock
+
+@author: Stijn De Weirdt (Ghent University)
+@author: Dries Verdegem (Ghent University)
+@author: Kenneth Hoste (Ghent University)
+@author: Pieter De Baets (Ghent University)
+@author: Jens Timmerman (Ghent University)
+"""
+
+from easybuild.framework.easyblock import EasyBlock
+
+
+class Toolchain(EasyBlock):
+    """
+    Compiler toolchain: generate module file only, nothing to build/install
+    """
+
+    def configure_step(self):
+        """Do nothing."""
+        pass
+
+    def build_step(self):
+        """Do nothing."""
+        pass
+
+    def install_step(self):
+        """Do nothing."""
+        pass
+
+    def sanity_check_step(self):
+        """
+        As a toolchain doesn't install anything really, this is always OK
+        """
+        pass
