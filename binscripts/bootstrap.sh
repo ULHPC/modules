270931ccddcd492236417df621faf3f8207f6878
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2015-03-19 10:47:12 +0100

    cleaning up repository

--- binscripts/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p $HOME/.resif/src && cd $HOME/.resif/src
-git clone https://gitlab.uni.lu/modules/infrastructure.git .
-sudo pip install $HOME/.resif/src/bin/
-sudo mkdir /usr/local/apps
-sudo chown $USER: /usr/local/apps
-resif cleaninstall
